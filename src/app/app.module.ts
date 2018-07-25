f7da0cb3725749190631d3ce5711c062915c74fa
andrewle95 <leaa1@uci.edu>
2018-07-24 21:59:40 -0700

    Content and pages work
    Need CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HittingPageComponent } from './hitting-page/hitting-page.component';
+import { AdvisorsPageComponent } from './advisors-page/advisors-page.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: 'advisors', component: AdvisorsPageComponent},
+  { path: 'hitting', component: HittingPageComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomePageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HittingPageComponent,
+    AdvisorsPageComponent
   ],
   imports: [
-    BrowserModule
+    MDBBootstrapModule.forRoot(),
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
