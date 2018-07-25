1978d671b31cea7a8991abe5904885734fd4ae3d
andrewle95 <leaa1@uci.edu>
2018-07-24 22:35:13 -0700

    Changing project name redirection

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/behavioradvice'));
+app.use(express.static(__dirname + '/dist/BehaviorAdvice'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/behavioradvice/index.html'));
+res.sendFile(path.join(__dirname+'/dist/BehaviorAdvice/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
